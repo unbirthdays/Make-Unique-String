6ec553063d4414f27357eca8d478137a3e048ab1
Sabrina Baccam <sabrinabaccam@gmail.com>
2022-10-04 23:16:39 -0500

    done

--- makeUnique.js
+++ makeUnique.js
@@ -1,1 +1,19 @@
 // Write your solution below:
+
+/* Write a function to remove all duplicate letters from a provided string.
+The string will only contain lowercase letters between a - z. The string will not contain spaces.
+*/
+
+function makeUnique(word) {
+    let newWord = '';
+    for(let i = 0; i < word.length; i++) {
+        if(!newWord.includes(word[i])) {
+            newWord += word[i];
+        }
+    }
+    return newWord;
+}
+
+
+console.log(makeUnique('helloworld')); // prints helowrd
+console.log(makeUnique('iwanttoclimbamountain')); // prints iwantoclmbmu
